d6860fe1e3f076d7f3ae1212c2f746fc2d471ef7
Cesar Salazar <rasecsv94@gmail.com>
2018-04-30 14:25:46 -0500

    rsc
    modificaciones de la pagina recursos

--- php/footer.php
+++ php/footer.php
@@ -8,55 +8,6 @@
                             <div class="iq-font-white iq-mt-15 ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
                         </div>
                     </div>
-              
-                   <!-- <div class="col-lg-2 col-md-6 col-sm-12 iq-mtb-60 footer-menu">
-                        <h5 class="small-title iq-tw-5 iq-font-white">Menu</h5>
-                        <ul class="iq-pl-0">
-                            <li><a href="# ">Home</a></li>
-                            <li><a href="# ">About Us</a></li>
-                            <li><a href="# ">Services</a></li>
-                            <li><a href="# ">Faqs</a></li>
-                            <li><a href="# ">Contact Us</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-12 iq-contact iq-mtb-60">
-                        <h5 class="small-title iq-tw-5 iq-font-white">Contact CoinEx</h5>
-                        <div class="iq-mb-30">
-                            <div class="blog"><i class="ion-ios-telephone-outline"></i>
-                                <div class="content ">
-                                    <div class="iq-tw-6 title ">Phone</div> +0123 456 789</div>
-                            </div>
-                        </div>
-                        <div class="iq-mb-30">
-                            <div class="blog "><i class="ion-ios-email-outline"></i>
-                                <div class="content ">
-                                    <div class="iq-tw-6 title ">Mail</div> mail@coinex.com</div>
-                            </div>
-                        </div>
-                        <div class="blog"><i class="ion-ios-location-outline"></i>
-                            <div class="content ">
-                                <div class="iq-tw-6 title ">Address</div> 1234 North Luke Lane, South Bend,IN 360001</div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-12 iq-mb-60">
-                        <div class="call-back">
-                            <h5 class="small-title iq-tw-5 iq-font-white">Request a Call Back</h5>
-                            <form>
-                                <div class="form-group iq-mb-20">
-                                    <input type="text" class="form-control" id="exampleInputName" placeholder="Enter Name">
-                                </div>
-                                <div class="form-group iq-mb-20">
-                                    <input type="text" class="form-control" id="exampleInputPhone" placeholder="Phone Number">
-                                </div>
-                                <div class="form-group iq-mb-20">
-                                    <input type="text" class="form-control" id="exampleInputsubject" placeholder="Subject">
-                                </div>
-                                <a class="button" href="#">Submit</a>
-                            </form>
-                        </div>
-                    </div>-->
-          
-                    
                 </div>
             </div>
         </div>
